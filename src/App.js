5ae897602422afb628f825d361b666bb21716bd7
Geovanny251201 <69869435+Geovanny251201@users.noreply.github.com>
2023-06-11 14:24:18 -0600

    segundo commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende react
         </a>
       </header>
     </div>
